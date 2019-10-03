89efe7fe9225d6e092c206a7a3f1e8fc05ef9ef6
naoto-leon <naoto.leon@gmail.com>
2019-10-03 12:14:58 +0900

    10/03 up

--- Unity_websoket/websoket_google/Assets/WSVoiceRecording.cs
+++ Unity_websoket/websoket_google/Assets/WSVoiceRecording.cs
@@ -10,10 +10,14 @@ public class WSVoiceRecording : MonoBehaviour
 
     private void Awake()
     {
+        //connect 
         ws_ = new WebSocket("ws://192.168.0.103:12002");
+
+        //On catch message event
         ws_.OnMessage += (sender, e) =>
         {
             Debug.Log(e.Data); // 認識結果
+            
 
         };
         ws_.Connect();
